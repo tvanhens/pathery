9206705c86ff2fabb22ad8b90845187ecdd30a66
Tyler van Hensbergen <tvanhens@gmail.com>
2022-11-21 11:10:19 -0700

    move query handler into pathery core

--- packages/pathery/src/service/index.rs
+++ packages/pathery/src/service/index.rs
@@ -1,12 +1,19 @@
+use crate::index::IndexLoader;
 use crate::lambda::http::{self, PatheryRequest};
 use crate::message::{WriterMessage, WriterSender};
 use crate::schema::{SchemaLoader, TantivySchema};
+
 use chrono::{DateTime, Utc};
-use serde;
+use json::Map;
+use serde::{self, Deserialize, Serialize};
 use serde_json as json;
 use serde_json::Value;
+use std::collections::HashMap;
 use std::time::SystemTime;
-use tantivy::Document;
+use tantivy::collector::TopDocs;
+use tantivy::query::QueryParser;
+use tantivy::schema::Field;
+use tantivy::{DocAddress, Document, Score, SnippetGenerator};
 use tracing;
 
 trait IndexResourceRequest {
@@ -99,10 +106,101 @@ pub async fn post_index(
     })
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct QueryRequest {
+    pub query: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SearchHit {
+    pub doc: json::Value,
+    pub snippets: HashMap<String, String>,
+    pub score: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct QueryResponse {
+    pub matches: Vec<SearchHit>,
+}
+
+pub async fn query_index(
+    index_loader: &dyn IndexLoader,
+    event: http::Request,
+) -> Result<http::Response<http::Body>, http::Error> {
+    let index_id = event.required_path_param("index_id");
+
+    let payload = match event.payload::<QueryRequest>() {
+        Ok(value) => value,
+        Err(err) => return err.into(),
+    };
+
+    let index = index_loader.load_index(&index_id);
+
+    let reader = index.reader().expect("Reader should load");
+
+    let searcher = reader.searcher();
+
+    let schema = index.schema();
+
+    let query_parser = QueryParser::for_index(
+        &index,
+        schema
+            .fields()
+            .filter(|(_, config)| config.is_indexed())
+            .map(|(field, _)| field)
+            .collect::<Vec<Field>>(),
+    );
+
+    let query = query_parser.parse_query(&payload.query)?;
+
+    let top_docs: Vec<(Score, DocAddress)> = searcher.search(&query, &TopDocs::with_limit(10))?;
+
+    let matches: Vec<_> = top_docs
+        .into_iter()
+        .map(|(score, address)| -> SearchHit {
+            let search_doc = searcher.doc(address).expect("doc should exist");
+            let mut doc_map = Map::new();
+            let mut snippets: HashMap<String, String> = HashMap::new();
+
+            for (field, entry) in index
+                .schema()
+                .fields()
+                .filter(|(_, entry)| entry.is_indexed())
+            {
+                let field_name = entry.name();
+                if let Some(value) = search_doc.get_first(field) {
+                    let value =
+                        json::to_value(value).expect("doc value should be JSON serializable");
+                    doc_map.insert(field_name.to_string(), value);
+
+                    let mut snippet_gen = SnippetGenerator::create(&searcher, &query, field)
+                        .unwrap_or_else(|_| {
+                            panic!("Unable to create snippet for field: {field_name}")
+                        });
+                    snippet_gen.set_max_num_chars(100);
+                    let snippet_text = snippet_gen.snippet_from_doc(&search_doc).to_html();
+                    if !snippet_text.is_empty() {
+                        snippets.insert(field_name.into(), snippet_text);
+                    }
+                }
+            }
+
+            SearchHit {
+                score,
+                doc: doc_map.into(),
+                snippets,
+            }
+        })
+        .collect();
+
+    http::success(&QueryResponse { matches })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{
+        index::TantivyIndex,
         message::{test_writer_sender, TestWriterSender},
         schema::SchemaProvider,
         tokio,
@@ -110,7 +208,13 @@ mod tests {
     use ::http::{Request, StatusCode};
     use aws_lambda_events::query_map::QueryMap;
     use lambda_http::{Body, RequestExt};
-    use std::collections::HashMap;
+    use serde::Deserialize;
+    use std::{collections::HashMap, sync::Arc};
+    use tantivy::{
+        doc,
+        schema::{self, Schema},
+        Index,
+    };
 
     fn setup() -> (TestWriterSender, SchemaProvider) {
         let config = json::json!({
@@ -146,9 +250,12 @@ mod tests {
         )
     }
 
-    fn parse_response(response: http::Response<http::Body>) -> (StatusCode, json::Value) {
+    fn parse_response<V>(response: http::Response<http::Body>) -> (StatusCode, V)
+    where
+        V: for<'de> Deserialize<'de>,
+    {
         let code = response.status();
-        let body: json::Value = if let Body::Text(x) = response.body() {
+        let body: V = if let Body::Text(x) = response.body() {
             json::from_str(x).unwrap()
         } else {
             panic!("Invalid body")
@@ -156,10 +263,8 @@ mod tests {
         (code, body)
     }
 
-    // Happy Path
-
     #[tokio::test]
-    async fn index_a_doc_with_no_id() {
+    async fn post_index_doc_with_no_id() {
         let (client, loader) = setup();
 
         let doc = json::json!({
@@ -171,15 +276,13 @@ mod tests {
 
         let response = post_index(&client, &loader, request).await.unwrap();
 
-        let (code, _body) = parse_response(response);
+        let (code, _body) = parse_response::<json::Value>(response);
 
         assert_eq!(code, 200);
     }
 
-    // Error States
-
     #[tokio::test]
-    async fn index_a_non_object() {
+    async fn post_index_non_object() {
         let (client, loader) = setup();
 
         let doc = json::json!([]);
@@ -188,14 +291,14 @@ mod tests {
 
         let response = post_index(&client, &loader, request).await.unwrap();
 
-        let (code, body) = parse_response(response);
+        let (code, body) = parse_response::<json::Value>(response);
 
         assert_eq!(code, 400);
         assert_eq!(body, json::json!({"message": "Expected a JSON object"}));
     }
 
     #[tokio::test]
-    async fn index_an_unsupported_value() {
+    async fn post_index_unsupported_value() {
         let (client, loader) = setup();
 
         let doc = json::json!({"foo": 1});
@@ -204,7 +307,7 @@ mod tests {
 
         let response = post_index(&client, &loader, request).await.unwrap();
 
-        let (code, body) = parse_response(response);
+        let (code, body) = parse_response::<json::Value>(response);
 
         assert_eq!(code, 400);
         assert_eq!(
@@ -214,7 +317,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn index_a_field_that_does_not_exist() {
+    async fn post_index_field_that_does_not_exist() {
         let (client, loader) = setup();
 
         let doc = json::json!({
@@ -225,10 +328,42 @@ mod tests {
 
         let response = post_index(&client, &loader, request).await.unwrap();
 
-        let (code, body) = parse_response(response);
+        let (code, body) = parse_response::<json::Value>(response);
 
         assert_eq!(code, 400);
         // Empty because the non-existent field does not explicitly trigger a failure - it just doesn't get indexed.
         assert_eq!(body, json::json!({"message": "Cannot index empty object"}));
+    }
+
+    #[tokio::test]
+    async fn query_document_with_un_indexed_fields() {
+        let mut schema = Schema::builder();
+        let title = schema.add_text_field("title", schema::STORED | schema::STRING);
+        let author = schema.add_text_field("author", schema::STORED);
+        let index = Index::create_in_ram(schema.build());
+        let mut writer = index.default_writer();
+
+        writer
+            .add_document(doc!(
+                title => "hello",
+                author => "world",
+            ))
+            .unwrap();
+
+        writer.commit().unwrap();
+
+        let request = request(
+            "test",
+            json::json!({
+                "query": "hello"
+            }),
+        );
+
+        let response = query_index(&Arc::new(index), request).await.unwrap();
+
+        let (status, body) = parse_response::<QueryResponse>(response);
+
+        assert_eq!(200, status);
+        assert_eq!(1, body.matches.len());
     }
 }
